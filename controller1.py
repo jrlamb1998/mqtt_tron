e1882f96b919e1af666508e1659d6f5dec7028ea
Jack Lamb <jlamb@lbl.gov>
2019-12-01 12:27:35 -0800

    partial implementation of IMU and button

--- controller1.py
+++ controller1.py
@@ -6,18 +6,43 @@ Jr Mints Final Project
 Jack Lamb and Rees Parker
 """
 
+from board import A21
+from machine import Pin
+import machine
+import time
+
+
+############# IMU STUFF #################
+from bno055 import *
+i2c = machine.I2C(1)
+# ESP8266 soft I2C
+# i2c = machine.I2C(-1, scl=machine.Pin(2), sda=machine.Pin(0))
+imu = BNO055(i2c)
+time.sleep(0.5)
+print(imu.accel())
+
+button = Pin(A21, mode=Pin.IN, pull=Pin.PULL_UP, debounce=500000)
+
+angle = 0 ##### angle of tilt calculated from the IMU, between 0 and 2pi
+ready = 0   ##### ready to play
+
+
+############# MQTT PUBLISH DATA #################
 from mqttclient import MQTTClient
-
-
-
 
 session = "jr_mints"
 BROKER = "mqtt.eclipse.org"
 mqtt = MQTTClient(BROKER)
 
-angle = 1.5 ##### angle of tilt calculated from the IMU, between 0 and 2pi
-ready = 1   ##### ready to play
+controller_topic = "{}/final/controller1".format(session)
 
-topic = "{}/final/controller1".format(session)
-data = str(angle) + "," + str(ready)
-mqtt.publish(topic, data)
+####################################
+
+############## LOGIC LOOP ###########
+
+while True:
+    if ready == 0:
+        ready = button()
+    data = str(angle) + "," + str(ready)
+    mqtt.publish(controller_topic, data)
+    time.sleep(25/1000)
