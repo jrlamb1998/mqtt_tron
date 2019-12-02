f6f85a6f98a77a7dd7be34db244d2e0154802395
Jack Lamb <jlamb@lbl.gov>
2019-12-02 10:57:31 -0800

    added wifi connection

--- controller1.py
+++ controller1.py
@@ -11,15 +11,72 @@ from machine import Pin
 import machine
 import time
 
+######################## BOOT AND GET WIFI ########################
+from network import WLAN, STA_IF
+from network import mDNS
+
+#esp_wifi_disconnect()
+#esp_wifi_stop()
+#espw_wifi_denit()
+
+wlan = WLAN(STA_IF)
+wlan.active(True)
+
+#wlan.connect('EE49-2.4', '122Hesse', 5000)
+wlan.connect('jack', 'z6thn815ikmnh', 5000)
+
+
+while not wlan.isconnected():
+    print("Waiting for wlan connection")
+    time.sleep(1)
+
+print("WiFi connected at", wlan.ifconfig()[0])
+
+# Advertise as 'hostname', alternative to IP address
+try:
+    hostname = 'jr_mints'
+    mdns = mDNS(wlan)
+    mdns.start(hostname, "MicroPython REPL")
+    mdns.addService('_repl', '_tcp', 23, hostname)
+    print("Advertised locally as {}.local".format(hostname))
+except OSError:
+    print("Failed starting mDNS server - already started?")
+
+# start telnet server for remote login
+from network import telnet
+
+print("start telnet server")
+telnet.start(user='jr_mints', password='jr_mints')
+
+# fetch NTP time
+from machine import RTC
+
+print("inquire RTC time")
+rtc = RTC()
+rtc.ntp_sync(server="pool.ntp.org")
+
+timeout = 10
+for _ in range(timeout):
+    if rtc.synced():
+        break
+    print("Waiting for rtc time")
+    time.sleep(1)
+
+if rtc.synced():
+    print(time.strftime("%c", time.localtime()))
+else:
+    print("could not get NTP time")
+    
+######################################################
+
 
 ############# IMU STUFF #################
-from bno055 import *
-i2c = machine.I2C(1)
-# ESP8266 soft I2C
-# i2c = machine.I2C(-1, scl=machine.Pin(2), sda=machine.Pin(0))
-imu = BNO055(i2c)
+from bno055_base import BNO055_BASE
+
+i2c = machine.I2C(-1, scl=machine.Pin(2), sda=machine.Pin(0))
+imu = BNO055_BASE(i2c)
 time.sleep(0.5)
-print(imu.accel())
+print(imu.euler())
 
 button = Pin(A21, mode=Pin.IN, pull=Pin.PULL_UP, debounce=500000)
 
