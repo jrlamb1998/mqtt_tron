cf5ec4ab4d09a58498acb45474776f4cb68af11d
Jack Lamb <jlamb@lbl.gov>
2019-12-12 17:36:29 -0800

    unknown edits

--- server.py
+++ server.py
@@ -104,23 +104,23 @@ while True:
             distance = np.sqrt( (player2_list[i,0]-x1)**2 + (player2_list[i,1] - y1)**2)
             if distance <= 0.9*float(speed):
                 gamestate = 2     
-                time.sleep(4)
+                time.sleep(2)
         for i in range(len(player1_list)-1):
             distance = np.sqrt( (player1_list[i,0]-x1)**2 + (player1_list[i,1] - y1)**2)
             if distance <= 0.9*float(speed):
                 gamestate = 2
-                time.sleep(4)
+                time.sleep(2)
                 
         for i in range(len(player1_list)):
             distance = np.sqrt( (player1_list[i,0]-x2)**2 + (player1_list[i,1] - y2)**2)
             if distance <= 0.9*speed:
                 gamestate = 1
-                time.sleep(4)
+                time.sleep(2)
         for i in range(len(player2_list)-1):
             distance = np.sqrt( (player2_list[i,0]-x2)**2 + (player2_list[i,1] - y2)**2)
             if distance <= 0.9*speed:
                 gamestate = 1
-                time.sleep(4)
+                time.sleep(2)
         
         player1_list = np.vstack((player1_list,[x1,y1]))
         player2_list = np.vstack((player2_list,[x2,y2]))
